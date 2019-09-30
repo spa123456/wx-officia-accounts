860d55e9d080949219bf9fa3a50be1094bc0be0c
shipingan <740425014@qq.com>
2019-09-30 09:19:28 +0800

    adsadasd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+      <a href="localhost:3000" target="_blank" rel="noopener"
         >vue-cli documentation</a
       >.
     </p>
